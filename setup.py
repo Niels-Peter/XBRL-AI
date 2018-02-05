68e53d7cca93a724f2e7bc9cca73941789cb196d
Benjamin <benjamin@soetang.dk>
2018-02-05 21:30:38 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from setuptools import setup
+
+setup(
+      name = "xbrl_ai",
+      version = "0.2",
+      description = "Making XRBL avaiable for Machine learning",
+      author = " ",
+      author_email = " ",
+      url = "https://github.com/Niels-Peter/XBRL-AI",
+      packages = ['xbrl_ai', 'xbrl_standard'],
+
+      long_description = "To be written",
+      classifiers = [
+              "Programming lanquage :: Python",
+              "License :: OSI Aproved :: MIT License",
+              "Operating system :: Linux"
+              ]
+      )
