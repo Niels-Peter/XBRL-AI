40d1311b4966cca057388c9c216cafa125a3ed3e
Niels-Peter Rønmos <nieron@erst.dk>
2018-02-07 21:29:15 +0100

    med datetiwith xbrl54

--- xbrl_ai/xbrl_ai.py
+++ xbrl_ai/xbrl_ai.py
@@ -98,3 +98,113 @@ def xbrlinstance_to_dict(xbrlinstance):
                 except LookupError:
                     pass
     return xbrldict
+
+
+def xbrldict_to_xbrl_54(xbrldict):
+
+    def get_xbrlkey(post, char):
+        return post[post.index(char)+1:]
+
+    def explicit_list(explicit):
+        explicit_liste = {}
+        dimension_list = []
+        label_extend = ''
+        if type(explicit).__name__ == 'OrderedDict':
+            explicit_liste[get_xbrlkey(explicit['@dimension'], ":")]\
+                = get_xbrlkey(explicit['$'], ":")
+        if type(explicit).__name__ == 'list':
+            for element in explicit:
+                explicit_liste[get_xbrlkey(element['@dimension'], ":")]\
+                    = get_xbrlkey(element['$'], ":")        
+        explicit_liste_od = collections.OrderedDict(sorted(explicit_liste.items()))
+        for keys in explicit_liste_od:
+            label_extend = label_extend + '_' + explicit_liste_od[keys]
+            dimension_list.append(keys)
+        return label_extend, dimension_list
+
+    def typed_list(typed):
+        typed_liste = {}
+        dimension_list = []
+        label_typed = label_typed_id = ''
+        if type(typed).__name__ == 'OrderedDict':
+            for poster in typed:
+                if poster == '@dimension':
+                    dimension = get_xbrlkey(typed['@dimension'], ":")
+                if poster != '@dimension':
+                    vaerdi = (typed[poster]).get('$', None)
+                    member = get_xbrlkey(poster, "}")
+            typed_liste[dimension, vaerdi] = member
+        if type(typed).__name__ == 'list':
+            for element in typed:
+                for poster in element:
+                    if poster == '@dimension':
+                        dimension = get_xbrlkey(element['@dimension'], ":")
+                    if poster != '@dimension':
+                        vaerdi = (element[poster]).get('$', None)
+                        member = get_xbrlkey(poster, "}")
+                typed_liste[dimension, vaerdi] = member
+        typed_liste_od = collections.OrderedDict(sorted(typed_liste.items()))
+        for keys in typed_liste_od:
+            dimension_list.append(keys[0])
+            label_typed = label_typed + '_' + typed_liste_od[keys]
+            if label_typed_id == '':
+                label_typed_id = str(keys[1])
+            else:
+                label_typed_id = label_typed_id + '|' + str(keys[1])
+        return label_typed, label_typed_id, dimension_list
+
+    def concept_data(inputdata):
+        value = inputdata.get('$', None)
+        unit = inputdata.get('unit', None)
+        decimals = inputdata.get('@decimals', None)
+        context = inputdata['context']
+        lang = inputdata.get('@{http://www.w3.org/XML/1998/namespace}lang', None)
+        startdate = context[2]
+        enddate = context[3]
+        instant = context[4]
+        if type(enddate).__name__ != 'str':
+            enddate = instant
+        explicit = context[5]
+        typed = context[6]
+        label_extend, dimension_list_extend = explicit_list(explicit)
+        label_typed, label_typed_id, dimension_list_typed = typed_list(typed)
+        dimension_list = dimension_list_extend.append(dimension_list_typed)
+        if label_typed_id == '':
+            label_typed_id = None
+        return value, unit, decimals, startdate, enddate, lang,\
+            label_extend, label_typed, label_typed_id, dimension_list_extend
+
+    #schemaRef = (XBRL['{http://www.xbrl.org/2003/linkbase}schemaRef'])\
+    #['@{http://www.w3.org/1999/xlink}href']
+    dict54 = {}
+    for post in xbrldict:
+        if post not in ('{http://www.xbrl.org/2003/linkbase}schemaRef',
+                        '@{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'):
+            ref = post[:post.index('}')]
+            xbrlref = ref[(ref.rfind('/') - len(ref) + 1):]
+            if type(xbrldict[post]).__name__ == 'list':
+                for element in xbrldict[post]:
+                    value, unit, decimals, startdate, enddate, lang, label_extend,\
+                        label_typed, label_typed_id, dimension_list = concept_data(element)
+                    concept = xbrlref + ':' + get_xbrlkey(post, '}') + label_extend + label_typed
+                    if type(unit).__name__ == 'NoneType':
+                        unit = lang
+                    nogle = (concept, startdate, enddate, label_typed_id, unit)
+                    if nogle in dict54 and dict54[nogle][0] != value:
+                        print('!!!!!!!!!', nogle, value, unit, decimals, dict54[nogle])
+                    if len(str(dimension_list)) < 5:
+                        dimension_list = None
+                    dict54[nogle] = [value, unit, decimals, dimension_list]
+            if type(xbrldict[post]).__name__ == 'OrderedDict':
+                value, unit, decimals, startdate, enddate, lang, label_extend, label_typed,\
+                    label_typed_id, dimension_list = concept_data(xbrldict[post])
+                concept = xbrlref + ':' + get_xbrlkey(post, '}') + label_extend + label_typed
+                if type(unit).__name__ == 'NoneType':
+                    unit = lang
+                nogle = (concept, startdate, enddate, label_typed_id, unit)
+                if nogle in dict54 and dict54[nogle][0] != value:
+                    print('!!!!!!!!!', nogle, value, unit, decimals, dict54[nogle])
+                if len(str(dimension_list)) < 5:
+                    dimension_list = None
+                dict54[nogle] = [value, unit, decimals, dimension_list]
+    return dict54
