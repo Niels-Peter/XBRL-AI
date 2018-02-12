d223abb372d19321a02ca4e31ca7e1803d7c408e
Niels-Peter Rønmos <nieron@erst.dk>
2018-02-12 16:35:56 +0100

    unit issue fix

---
+++ example/test_xbrl_ai_us.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 16:25:49 2018
+
+@author: niels-peter
+"""
+
+import requests
+from xbrl_ai import xbrlinstance_to_dict, xbrldict_to_xbrl_54
+from xbrl_local.xbrl_ai_dk import fetchlist_dk, xbrldict_to_xbrl_dk_64, xbrl_dk_64_to_xbrl_dk_11, xbrldocs_to_dict
+
+
+__title__ = 'test_xbrl_ai_us'
+__version__ = '0.0.1'
+__author__ = 'Niels-Peter Rønmos'
+
+file = open('goog-20151231.xml','r')
+file_indhold = file.read()
+
+xbrl = xbrlinstance_to_dict(file_indhold)
+xbrl64 = xbrldict_to_xbrl_54(xbrl)
+
